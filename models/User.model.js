6f50775722db97c34f690325fca4d8392aabdb3c
Lucia de Haro <elle.dharo@gmail.com>
2022-05-09 12:23:34 +0200

    prueba

--- models/User.model.js
+++ models/User.model.js
@@ -1,18 +1,77 @@
 const { Schema, model } = require("mongoose");
 
-// TODO: Please make sure you edit the user model to whatever makes sense in this case
 const userSchema = new Schema(
   {
-    username: {
+    name: {
       type: String,
-      // unique: true -> Ideally, should be unique, but its up to you
+      required: [true, 'El nombre de usuario es obligatorio'],
+      trim: true
     },
-    password: String,
-  },
-  {
-    // this second object adds extra properties: `createdAt` and `updatedAt`
-    timestamps: true,
+    surname: {
+      type: String,
+      required: [true, 'El nombre de usuario es obligatorio'],
+      trim: true
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: [true, 'El email es obligatorio'],
+      lowercase: true,
+      trim: true
+    },
+    password: {
+      type: String,
+      required: [true, 'La contraseña es obligatoria']
+    },
+    profileImg: { 
+      type: String, 
+      default: 'https://i.stack.imgur.com/l60Hf.png' 
+      },
+  
+    description: {
+      type: String,
+      required: true, 
+    },
+    ocupacion: {
+        type : string,
+        enum: [Digital, Education, Marketing, Events, Diseño, Foto, otros ]
+      },
+  
+    role: {
+      type: String, 
+      enum: ['USER', 'ADMIN', 'PUBLISHER'], 
+      default: 'USER'
+    },
+      
+    match :  [
+             {
+                 type: Schema.Types.ObjectId,
+                 ref:"user",
+             },
+         ], 
+  
+    publisher :  [
+             {
+                 type: Schema.Types.ObjectId,
+                 ref:"publisher",
+             },
+         ],  
+  
+    eventAssitance :  [
+             {
+                 type: Schema.Types.ObjectId,
+                 ref:"event",
+             },
+         ],   
+  
+     coursesAssitance :  [
+             {
+                 type: Schema.Types.ObjectId,
+                 ref:"course",
+             },
+         ],              
   }
+  
 );
 
 const User = model("User", userSchema);
